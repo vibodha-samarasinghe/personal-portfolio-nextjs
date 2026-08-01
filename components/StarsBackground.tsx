05e1a05ce67c459b6e351eafdd569a8784b1e662
Vibodha Samarasinghe <vibodhasamarasinghe@Vibodhas-MacBook-Air.local>
2026-08-01 09:31:01 +0530

    Add dark glass star background

---
+++ components/StarsBackground.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function StarsBackground() {
+  return (
+    <div className="stars-container">
+
+      <div className="stars"></div>
+
+      <div className="stars-small"></div>
+
+      <div className="shooting-star"></div>
+
+    </div>
+  );
+}
